0e922ddaa5db3ed728d036e496ca21473c858eed
Dmytro <dmazyar@gmail.com>
2017-12-02 00:44:21 +0100

    added cucumber allure reporter

--- reporter/testReporter2.js
+++ reporter/testReporter2.js
@@ -1,18 +1,42 @@
 let util = require('util'),
     events = require('events');
 let Allure = require('allure-js-commons');
-let allure = Object.create(Allure);
-
+let allure = new Allure();
+let id =0;
 
 let testReporter2 = function(options) {
+    allure.setOptions({ targetDir: options.outputDir || 'allure-results' })
+
     this.on('suite:start', function(suit) {
-      console.log('Suit start');
-      allure.getCurrentSuite(suit.cid);
-      console.log(suit)
+        if (suit.parent === null) {
+            console.log('Suit')
+            allure.startSuite(suit.title);
+        } else {
+            console.log('Case')
+            allure.startCase(suit.title+id);
+        }
+        id++;
+        console.log('Suit start');
     });
 
-    this.on('suite:end', function() {
-      console.log('Suit end');
+    this.on('suite:end', function(suit) {
+        console.log('Suit end');
+        if (suit.parent === null) {
+            allure.endSuite();
+        } else {
+            allure.endCase()
+        }
+    });
+
+    this.on('test:start', function (test) {
+        test.parent = 'ddfgdfgdg'
+        console.log('This is a step'+test.parent)
+        allure.startStep(test.title)
+        allure.endStep()
+    });
+
+    this.on('test:end', function (){
+
     });
 };
 
